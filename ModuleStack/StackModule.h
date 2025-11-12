ba32e5529a80b1758b1c7e9effd460bb0ba3d670
Michael Luis Condori Paye <mcondoripaye@frba.utn.edu.ar>
2025-11-12 02:42:19 +0000

    Tercer Commit.

--- ModuleStack/StackModule.h
+++ ModuleStack/StackModule.h
@@ -4,3 +4,23 @@ b. Pop.
 c. IsEmpty.
 d. IsFull.
 */
+#ifndef STACKMODULE_H
+#define STACKMODULE_H
+
+#define CANTIDADMAX 4
+typedef int StackItem;
+
+
+typedef struct StackModule{
+    StackItem* Stack;
+    int Top;
+}StackModule;
+
+
+void  Push (StackModule* st, StackItem item);
+
+void Pop (StackModule* st);
+
+int IsEmpty (StackModule* st);
+int IsFull (StackModule* st);
+#endif 
--- ModuleStack/StackModuleContiguousStatic.c
+++ ModuleStack/StackModuleContiguousStatic.c
@@ -3,22 +3,72 @@
 #include <stdlib.h>
 
 
+StackModule* mystack= NULL;
 
-
-
-int main()
-{
-    printf("HEllo");
-    StackModule* mystack = malloc(sizeof(StackModule));
+void inicializacionStack(){
+    mystack = malloc(sizeof(StackModule));
     mystack->Top=0;
 
     StackItem* stackArray= (StackItem *) malloc(CANTIDADMAX * sizeof (StackItem));
     mystack->Stack= stackArray;
+    StackItem *p = mystack->Stack;
     
-    int *p = mystack->Stack;
     (*p) = 10;
     mystack->Top++;
-    printf("First element in stack: %d\n", (*p));
+}
 
+void Push(StackModule* stackModule, StackItem item){
+    StackItem *p = stackModule->Stack;
+    if (IsFull(stackModule)){
+        printf("Stack Overflow\n");
+    }else
+    {
+        *(p + stackModule->Top) = item;
+        stackModule->Top++;
+    }
+    
+}
+
+static void mostraStackContinuo(StackModule* stackModule){
+    int *p= stackModule->Stack;
+    for (int i = 0; i < stackModule->Top; i++)
+    {
+       printf("Element %d:%d\n",i, *(p+i));
+    }
+    
+}
+
+int IsFull(StackModule* StackModule){
+    return StackModule->Top >= CANTIDADMAX ? 1: 0;
+}
+void Pop(StackModule* stackModule){
+    StackItem *p = stackModule->Stack;
+    if(IsEmpty(stackModule)){
+        printf("EL Stack esta vacio\n");
+    }else{
+        printf("top antes de pop %d\n",stackModule->Top);
+        printf("Elemento %d eliminado \n",(*(p + stackModule->Top-1)));
+        *(p + stackModule ->Top) = 0;
+        stackModule->Top--;
+    }
+}
+
+int IsEmpty(StackModule* stackModule){
+    return stackModule -> Top <= 0? 1:0;
+}
+int main()
+{ 
+    inicializacionStack();
+    Push(mystack,10);
+    Push(mystack,20);
+    Push(mystack,30);
+    Pop(mystack);
+    Push(mystack,40);
+    printf("Is Full: %d \n",IsFull(mystack));
+    Pop(mystack);
+    Pop(mystack);
+    Pop(mystack);
+    Pop(mystack);
+    mostraStackContinuo(mystack);
     return 0;
 }
